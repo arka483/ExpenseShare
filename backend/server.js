acbc22eb13348b1a4182a139148ecea718df5d5a
Rishav <rajrishav011@gmail.com>
2025-12-23 22:30:24 +0530

    add server file

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/groups', require('./routes/groupRoutes'));
+app.use('/api/expenses', require('./routes/expenseRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
